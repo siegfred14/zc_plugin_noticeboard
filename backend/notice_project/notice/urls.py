149243d19c242d09cb49a9ee776551d0f497461d
Siegfred Samson <siegfredsamson@gmail.com>
2021-09-17 19:15:54 +0100

    update-notice endpoint created

--- backend/notice_project/notice/urls.py
+++ backend/notice_project/notice/urls.py
@@ -1,42 +1,36 @@
 from django.urls import path
-<<<<<<< HEAD
-from .views import install, sidebar, CreateNewNotices, NoticeAPI, UpdateNoticeAPIView, DeleteNotice, search
-=======
-from .views import install,sidebar, create_room, CreateNewNotices, search, get_room
+from .views import install, sidebar, create_room, CreateNewNotices, UpdateNoticeAPIView, search, get_room
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
->>>>>>> 1a859a53a498550d1f4a5b9b770414dea004d3d1
-
 
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Noticeboard API",
-      default_version='v1',
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Noticeboard API",
+        default_version='v1',
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
-   
-   path('sidebar', sidebar, name="sidebar"),
 
-   path('install',install, name='install'),
+    path('sidebar', sidebar, name="sidebar"),
 
-<<<<<<< HEAD
     path('install', install, name='install'),
-=======
-   path('create-notice-room', create_room),
->>>>>>> 1a859a53a498550d1f4a5b9b770414dea004d3d1
 
-   path('create-notice', CreateNewNotices.as_view()),
+    path('create-notice-room', create_room),
 
-   path('search', search.as_view()),
+    path('create-notice', CreateNewNotices.as_view()),
 
-   path('get-room', get_room),
+    path('update-notice', UpdateNoticeAPIView.as_view()),
 
-   path('docs', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('search', search.as_view()),
+
+    path('get-room', get_room),
+
+    path('docs', schema_view.with_ui(
+        'swagger', cache_timeout=0), name='schema-swagger-ui'),
 
 ]
